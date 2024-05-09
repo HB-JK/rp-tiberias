886f1668c29915c9da388154c448d038b553d1f7
Julius Kayne <juliuskayne07@gmail.com>
2024-05-09 14:02:45 +0700

    update(visi-misi-images): lower the image size

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <>
             <header
                 id="home"
-                className="bg-hero bg-no-repeat bg-center bg-cover lg:h-screen md:h-[90vh] sm:h-[80vh] h-[50vh] flex items-center"
+                className="bg-hero bg-no-repeat bg-center bg-cover md:h-[90vh] sm:h-[80vh] h-[50vh] flex items-center"
             >
                 <div className="main-container hero">
                     <h4 className="text-center text-white lg:text-4xl md:text-2xl sm:text-xl text-sm font-medium">
@@ -74,11 +74,30 @@ export default function Home() {
                         </div>
                     </div>
 
-                    <div className="visi-misi-images md:w-1/2 order-2">
-                        <img
-                            src="/assets/visi-misi.png"
-                            alt="Visi misi image"
-                        />
+                    <div className="md:w-1/2 w-full h-[100%] order-2 relative">
+                        <div className="relative w-full 2xl:h-[31rem] xl:h-[30rem] lg:h-[24rem] lg:block flex flex-wrap">
+                            {/* decoration */}
+                            <div className="circle xl:w-[67px] lg:w-[55px] xl:h-[67px] lg:h-[55px] bg-light-secondary rounded-full absolute lg:block hidden bottom-20 left-5"></div>
+                            <div className="circle xl:w-[57px] lg:w-[45px] xl:h-[57px] lg:h-[45px] bg-secondary rounded-full absolute lg:block hidden bottom-0 right-[19%]"></div>
+                            <div className="circle xl:w-[47px] lg:w-[35px] xl:h-[47px] lg:h-[35px] bg-primary rounded-full absolute lg:block hidden top-8 right-[6%]"></div>
+                            
+                            {/* images */}
+                            <div className="2xl:w-44 xl:w-32 lg:w-28 w-5/12 h-auto lg:absolute xl:right-28 lg:right-20 top-0 lg:rounded-l-3xl lg:rounded-br-3xl rounded-sm object-cover lg:overflow-hidden">
+                                <img src="/assets/gallery/img-4.webp" alt="image" className="p-1 w-full h-full lg:aspect-square aspect-[4/3] object-cover" />
+                            </div>
+                            
+                            <div className="2xl:w-80 xl:w-[17rem] lg:w-52 w-7/12 h-auto lg:absolute left-0 top-0 lg:rounded-l-3xl lg:rounded-br-3xl rounded-sm object-cover lg:overflow-hidden">
+                                <img src="/assets/gallery/img-2.webp" alt="image" className="p-1 w-full h-full aspect-[4/3] object-cover" />
+                            </div>
+                            
+                            <div className="2xl:w-48 xl:w-40 lg:w-32 w-7/12 h-auto lg:absolute xl:left-28 lg:left-20 xl:bottom-10 lg:bottom-16 lg:rounded-r-3xl lg:rounded-tl-3xl rounded-sm object-cover lg:overflow-hidden">
+                                <img src="/assets/gallery/custom-img-5.webp" alt="image" className="p-1 w-full h-full lg:aspect-square aspect-[4/3] object-cover" />
+                            </div>
+                            
+                            <div className="2xl:w-72 xl:w-60 lg:w-48 w-5/12 h-auto lg:absolute right-0 bottom-20 lg:rounded-l-3xl lg:rounded-tr-3xl rounded-sm object-cover lg:overflow-hidden">
+                                <img src="/assets/gallery/img-1.webp" alt="image" className="p-1 w-full h-full aspect-[4/3] object-cover" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -96,7 +115,7 @@ export default function Home() {
 
                     <div className="flex flex-wrap md:flex-nowrap justify-between">
                         {list_member.map((value) => (
-                            <div className="md:w-1/3 sm:w-1/2 w-full p-3 flex flex-col items-center gap-2">
+                            <div className="md:w-1/3 sm:w-1/2 w-full p-3 flex flex-col items-center gap-2" key={value.id}>
                                 <div className="member-img sm:w-5/6 w-2/3">
                                     <img
                                         src={value.img}
